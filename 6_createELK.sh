b4f9c5e99653b910193e95628925a8f32b842b7d
Marius Keppler <mkeppler@raxio.eu>
2020-05-24 21:38:29 +0200

    sh files to create ELK and start transformation apps

---
+++ 6_createELK.sh
@@ -0,0 +1,5 @@
+./k8s/k8s.sh k8s/elastic-sts.yml
+./k8s/k8s.sh k8s/kibana-deploy.yml
+./k8s/k8s.sh k8s/logstash-raw.yml
+./k8s/k8s.sh k8s/logstash-view.yml
+./k8s/k8s.sh k8s/logstash-view-key.yml
---
+++ 7_transformationApps.sh
@@ -0,0 +1,2 @@
+./k8s/k8s.sh k8s/build-transform-app.yml
+./k8s/k8s.sh k8s/duration-transform-app.yml
