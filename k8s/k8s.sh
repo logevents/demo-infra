a1fb0ae2b4382c73db6580ab8c8634b94cf259ce
Marius Keppler <mkeppler@raxio.eu>
2020-05-24 10:43:21 +0200

    we also transform filebeat index to elastic

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -6,7 +6,7 @@ export CURRENT_TIME=$(date +%Y-%M-%dT%H-%M-%S)
 export KAFKA_SERVERS="kafka-0.kafka:9092"
 export RAW_TOPIC_PATTERN=filebeat-1
 export LOGEVENT_TOPIC=logevent-1
-export LOGSTASH_VIEW_PATTERN=logevent-1
+export LOGSTASH_VIEW_PATTERN="filebeat-1|logevent-1"
 export LOGSTASH_VIEW_KEY_PATTERN="build-1|duration-1"
 export ELASTIC_HOST=elastic
 
