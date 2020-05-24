ced1d60cefb37e9df43c30d900c41997afa1f233
Fabian Fichter <faficode@gmail.com>
2020-05-24 10:12:11 +0200

    duration extensions

--- k8s/k8s.sh
+++ k8s/k8s.sh
@@ -7,7 +7,7 @@ export KAFKA_SERVERS="kafka-0.kafka:9092"
 export RAW_TOPIC_PATTERN=filebeat-1
 export LOGEVENT_TOPIC=logevent-1
 export LOGSTASH_VIEW_PATTERN=logevent-1
-export LOGSTASH_VIEW_KEY_PATTERN=build-1
+export LOGSTASH_VIEW_KEY_PATTERN="build-1|duration-1"
 export ELASTIC_HOST=elastic
 
 cat $1 | envsubst | kubectl apply -f -
