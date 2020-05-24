d703b0c043ef52fb894bc6203f5cbc9a65b82d61
Marius Keppler <mkeppler@raxio.eu>
2020-05-24 10:43:05 +0200

    configmap will be applied correctly. Builds have quietperiod

--- 2_createConfigMapFilebeat.sh
+++ 2_createConfigMapFilebeat.sh
@@ -1,1 +1,1 @@
-kubectl create configmap jenkins-filebeat-minimal --from-file=minimalFilebeatConfig.yml
+kubectl create configmap jenkins-filebeat --from-file=k8s/filebeat-cm.yml -o yaml --dry-run | kubectl apply -f -
